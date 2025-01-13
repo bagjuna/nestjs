5c6c6e1609842b632d06ed2b335d25e4c2e24c01
juna3437@naver.com <juna3437@naver.com>
2025-01-13 09:26:44 +0900

    first_db(user)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-
+import {User} from "./users/uesr.entity";
+import {Report} from "./reports/report.entity";
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [],
-    synchronize: true,
-  }),UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true, // true로 설정하면 데이터베이스가 자동으로 생성/동기화됨
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-
